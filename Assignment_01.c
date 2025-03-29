7253c681039a933f61c0d03b60e8e96f58a8d375
Koogain <koogain0107@gmail.com>
2025-03-29 22:11:30 +0900

    Add files via upload

---
+++ Assignment_01.c
@@ -0,0 +1,21 @@
+/* 파일명: Assignment_01.c
+
+ * 내용: 다음과 같이 출력하는 프로그램을 작성하시오. 단, printf문은 한번만 사용한다.
+
+ * 작성자: 구가인
+
+ * 날짜: 2025.3.29
+
+ * 버전: v1.0
+
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("int main(void)\n{\n   return 0;\n}");
+
+		return 0;
+
+}
