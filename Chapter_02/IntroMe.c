84daf6be231d0b1130ef52f9b1c37d327802ccbc
Koogain <koogain0107@gmail.com>
2025-03-29 22:20:26 +0900

    Add files via upload

--- Chapter_02/IntroMe.c
+++ Chapter_02/IntroMe.c
@@ -1,3 +1,15 @@
+/* 파일명: IntroMe.c
+
+ * 내용: Visual Studio를 이용해서 다음과 같이 C 프로그램을 작성하고, 빌드 후 실행하시오.
+
+ * 작성자: 구가인
+
+ * 날짜: 2025.3.
+
+ * 버전: v1.0
+
+ */
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
